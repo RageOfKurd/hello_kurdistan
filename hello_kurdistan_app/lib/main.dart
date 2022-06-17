542556ae0664b8cfcd639dcceaf7b8c640b415b9
rageofkurd <rageofkurd98@gmail.com>
2022-06-17 12:10:49 +0300

    a network image added with a text

--- hello_kurdistan_app/lib/main.dart
+++ hello_kurdistan_app/lib/main.dart
@@ -11,11 +11,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kurdistan App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(body: Center(child: Text("hello Kurdsitan"))),
+      home: Scaffold(
+          appBar: AppBar(title: Center(child: Text("Kurdistan"))),
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 40, 10, 10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(25),
+                      child: Image.network(
+                          "https://preview.redd.it/9zgkmvxau9h81.jpg?auto=webp&s=c49655b5b779a4dff0815065c475561c62ff7980",
+                          width: 250),
+                    ),
+                  ), //
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(32, 10, 10, 10),
+                    child: const Text("Hello Kurdistan",
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
